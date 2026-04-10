7cf3acd437cd63381b404e84ecc8fe6ed201b6b7
mark.smit <mark.smit@cloudnation.nl>
2026-04-10 16:48:17 +0200

    fix: align for_each patterns to wam conventions

--- main.tf
+++ main.tf
@@ -5,17 +5,17 @@ resource "azurerm_dev_center" "this" {
   for_each = var.config.dev_center != null ? { "default" = var.config.dev_center } : {}
 
   name                              = each.value.name
-  resource_group_name               = coalesce(try(each.value.resource_group_name, null), var.resource_group_name, var.config.resource_group_name)
-  location                          = coalesce(try(each.value.location, null), var.location, var.config.location)
-  tags                              = coalesce(try(each.value.tags, null), var.tags)
+  resource_group_name               = coalesce(each.value.resource_group_name, var.resource_group_name, var.config.resource_group_name)
+  location                          = coalesce(each.value.location, var.location, var.config.location)
+  tags                              = coalesce(each.value.tags, var.tags)
   project_catalog_item_sync_enabled = each.value.project_catalog_item_sync_enabled
 
   dynamic "identity" {
-    for_each = try(each.value.identity, null) != null ? [each.value.identity] : []
+    for_each = each.value.identity != null ? [each.value.identity] : []
 
     content {
       type         = identity.value.type
-      identity_ids = try(identity.value.identity_ids, [])
+      identity_ids = identity.value.identity_ids
     }
   }
 }
@@ -25,23 +25,23 @@ resource "azurerm_dev_center_project" "this" {
   for_each = var.config.dev_center_project != null ? { "default" = var.config.dev_center_project } : {}
 
   name                       = each.value.name
-  resource_group_name        = coalesce(try(each.value.resource_group_name, null), var.resource_group_name, var.config.resource_group_name)
-  location                   = coalesce(try(each.value.location, null), var.location, var.config.location)
+  resource_group_name        = coalesce(each.value.resource_group_name, var.resource_group_name, var.config.resource_group_name)
+  location                   = coalesce(each.value.location, var.location, var.config.location)
   description                = each.value.description
-  tags                       = coalesce(try(each.value.tags, null), var.tags)
+  tags                       = coalesce(each.value.tags, var.tags)
   maximum_dev_boxes_per_user = each.value.maximum_dev_boxes_per_user
 
   dev_center_id = coalesce(
-    try(each.value.dev_center_id, null),
+    each.value.dev_center_id,
     try(azurerm_dev_center.this["default"].id, null)
   )
 
   dynamic "identity" {
-    for_each = try(each.value.identity, null) != null ? [each.value.identity] : []
+    for_each = each.value.identity != null ? [each.value.identity] : []
 
     content {
       type         = identity.value.type
-      identity_ids = try(identity.value.identity_ids, [])
+      identity_ids = identity.value.identity_ids
     }
   }
 }
@@ -62,71 +62,71 @@ resource "azurerm_managed_devops_pool" "this" {
   work_folder         = var.config.work_folder
 
   dynamic "identity" {
-    for_each = var.config.identity != null ? { this = {} } : {}
+    for_each = var.config.identity != null ? [var.config.identity] : []
     content {
-      type         = var.config.identity.type
-      identity_ids = var.config.identity.identity_ids
+      type         = identity.value.type
+      identity_ids = identity.value.identity_ids
     }
   }
 
   dynamic "stateless_agent" {
-    for_each = var.config.stateless_agent != null ? { this = {} } : {}
+    for_each = var.config.stateless_agent != null ? [var.config.stateless_agent] : []
     content {
       dynamic "automatic_resource_prediction" {
-        for_each = var.config.stateless_agent.automatic_resource_prediction != null ? { this = {} } : {}
+        for_each = stateless_agent.value.automatic_resource_prediction != null ? [stateless_agent.value.automatic_resource_prediction] : []
         content {
-          prediction_preference = var.config.stateless_agent.automatic_resource_prediction.prediction_preference
+          prediction_preference = automatic_resource_prediction.value.prediction_preference
         }
       }
       dynamic "manual_resource_prediction" {
-        for_each = var.config.stateless_agent.manual_resource_prediction != null ? { this = {} } : {}
+        for_each = stateless_agent.value.manual_resource_prediction != null ? [stateless_agent.value.manual_resource_prediction] : []
         content {
-          time_zone_name    = var.config.stateless_agent.manual_resource_prediction.time_zone_name
-          all_week_schedule = var.config.stateless_agent.manual_resource_prediction.all_week_schedule
+          time_zone_name    = manual_resource_prediction.value.time_zone_name
+          all_week_schedule = manual_resource_prediction.value.all_week_schedule
           dynamic "monday_schedule" {
-            for_each = var.config.stateless_agent.manual_resource_prediction.monday_schedule
+            for_each = manual_resource_prediction.value.monday_schedule
             content {
               count = monday_schedule.value.count
               time  = monday_schedule.value.time
             }
           }
           dynamic "tuesday_schedule" {
-            for_each = var.config.stateless_agent.manual_resource_prediction.tuesday_schedule
+            for_each = manual_resource_prediction.value.tuesday_schedule
             content {
               count = tuesday_schedule.value.count
               time  = tuesday_schedule.value.time
             }
           }
           dynamic "wednesday_schedule" {
-            for_each = var.config.stateless_agent.manual_resource_prediction.wednesday_schedule
+            for_each = manual_resource_prediction.value.wednesday_schedule
             content {
               count = wednesday_schedule.value.count
               time  = wednesday_schedule.value.time
             }
           }
           dynamic "thursday_schedule" {
-            for_each = var.config.stateless_agent.manual_resource_prediction.thursday_schedule
+            for_each = manual_resource_prediction.value.thursday_schedule
             content {
               count = thursday_schedule.value.count
               time  = thursday_schedule.value.time
             }
           }
           dynamic "friday_schedule" {
-            for_each = var.config.stateless_agent.manual_resource_prediction.friday_schedule
+            for_each = manual_resource_prediction.value.friday_schedule
             content {
               count = friday_schedule.value.count
               time  = friday_schedule.value.time
             }
           }
           dynamic "saturday_schedule" {
-            for_each = var.config.stateless_agent.manual_resource_prediction.saturday_schedule
+            for_each = manual_resource_prediction.value.saturday_schedule
             content {
               count = saturday_schedule.value.count
               time  = saturday_schedule.value.time
             }
           }
           dynamic "sunday_schedule" {
-            for_each = var.config.stateless_agent.manual_resource_prediction.sunday_schedule
+            for_each = manual_resource_prediction.value.sunday_schedule
             content {
               count = sunday_schedule.value.count
               time  = sunday_schedule.value.time
@@ -138,65 +138,65 @@ resource "azurerm_managed_devops_pool" "this" {
   }
 
   dynamic "stateful_agent" {
-    for_each = var.config.stateful_agent != null ? { this = {} } : {}
+    for_each = var.config.stateful_agent != null ? [var.config.stateful_agent] : []
     content {
-      grace_period_time_span = var.config.stateful_agent.grace_period_time_span
-      maximum_agent_lifetime = var.config.stateful_agent.maximum_agent_lifetime
+      grace_period_time_span = stateful_agent.value.grace_period_time_span
+      maximum_agent_lifetime = stateful_agent.value.maximum_agent_lifetime
       dynamic "automatic_resource_prediction" {
-        for_each = var.config.stateful_agent.automatic_resource_prediction != null ? { this = {} } : {}
+        for_each = stateful_agent.value.automatic_resource_prediction != null ? [stateful_agent.value.automatic_resource_prediction] : []
         content {
-          prediction_preference = var.config.stateful_agent.automatic_resource_prediction.prediction_preference
+          prediction_preference = automatic_resource_prediction.value.prediction_preference
         }
       }
       dynamic "manual_resource_prediction" {
-        for_each = var.config.stateful_agent.manual_resource_prediction != null ? { this = {} } : {}
+        for_each = stateful_agent.value.manual_resource_prediction != null ? [stateful_agent.value.manual_resource_prediction] : []
         content {
-          time_zone_name    = var.config.stateful_agent.manual_resource_prediction.time_zone_name
-          all_week_schedule = var.config.stateful_agent.manual_resource_prediction.all_week_schedule
+          time_zone_name    = manual_resource_prediction.value.time_zone_name
+          all_week_schedule = manual_resource_prediction.value.all_week_schedule
           dynamic "monday_schedule" {
-            for_each = var.config.stateful_agent.manual_resource_prediction.monday_schedule
+            for_each = manual_resource_prediction.value.monday_schedule
             content {
               count = monday_schedule.value.count
               time  = monday_schedule.value.time
             }
           }
           dynamic "tuesday_schedule" {
-            for_each = var.config.stateful_agent.manual_resource_prediction.tuesday_schedule
+            for_each = manual_resource_prediction.value.tuesday_schedule
             content {
               count = tuesday_schedule.value.count
               time  = tuesday_schedule.value.time
             }
           }
           dynamic "wednesday_schedule" {
-            for_each = var.config.stateful_agent.manual_resource_prediction.wednesday_schedule
+            for_each = manual_resource_prediction.value.wednesday_schedule
             content {
               count = wednesday_schedule.value.count
               time  = wednesday_schedule.value.time
             }
           }
           dynamic "thursday_schedule" {
-            for_each = var.config.stateful_agent.manual_resource_prediction.thursday_schedule
+            for_each = manual_resource_prediction.value.thursday_schedule
             content {
               count = thursday_schedule.value.count
               time  = thursday_schedule.value.time
             }
           }
           dynamic "friday_schedule" {
-            for_each = var.config.stateful_agent.manual_resource_prediction.friday_schedule
+            for_each = manual_resource_prediction.value.friday_schedule
             content {
               count = friday_schedule.value.count
               time  = friday_schedule.value.time
             }
           }
           dynamic "saturday_schedule" {
-            for_each = var.config.stateful_agent.manual_resource_prediction.saturday_schedule
+            for_each = manual_resource_prediction.value.saturday_schedule
             content {
               count = saturday_schedule.value.count
               time  = saturday_schedule.value.time
             }
           }
           dynamic "sunday_schedule" {
-            for_each = var.config.stateful_agent.manual_resource_prediction.sunday_schedule
+            for_each = manual_resource_prediction.value.sunday_schedule
             content {
               count = sunday_schedule.value.count
               time  = sunday_schedule.value.time
@@ -233,16 +233,16 @@ resource "azurerm_managed_devops_pool" "this" {
     }
 
     dynamic "security" {
-      for_each = var.config.virtual_machine_scale_set_fabric.security != null ? { this = {} } : {}
+      for_each = var.config.virtual_machine_scale_set_fabric.security != null ? [var.config.virtual_machine_scale_set_fabric.security] : []
       content {
-        interactive_logon_enabled = var.config.virtual_machine_scale_set_fabric.security.interactive_logon_enabled
+        interactive_logon_enabled = security.value.interactive_logon_enabled
         dynamic "key_vault_management" {
-          for_each = var.config.virtual_machine_scale_set_fabric.security.key_vault_management != null ? { this = {} } : {}
+          for_each = security.value.key_vault_management != null ? [security.value.key_vault_management] : []
           content {
-            key_vault_certificate_ids  = var.config.virtual_machine_scale_set_fabric.security.key_vault_management.key_vault_certificate_ids
-            certificate_store_location = var.config.virtual_machine_scale_set_fabric.security.key_vault_management.certificate_store_location
-            certificate_store_name     = var.config.virtual_machine_scale_set_fabric.security.key_vault_management.certificate_store_name
-            key_export_enabled         = var.config.virtual_machine_scale_set_fabric.security.key_vault_management.key_export_enabled
+            key_vault_certificate_ids  = key_vault_management.value.key_vault_certificate_ids
+            certificate_store_location = key_vault_management.value.certificate_store_location
+            certificate_store_name     = key_vault_management.value.certificate_store_name
+            key_export_enabled         = key_vault_management.value.key_export_enabled
           }
         }
       }
@@ -260,14 +260,14 @@ resource "azurerm_managed_devops_pool" "this" {
     }
 
     dynamic "permission" {
-      for_each = var.config.azure_devops_organization.permission != null ? { this = {} } : {}
+      for_each = var.config.azure_devops_organization.permission != null ? [var.config.azure_devops_organization.permission] : []
       content {
-        kind = var.config.azure_devops_organization.permission.kind
+        kind = permission.value.kind
         dynamic "administrator_account" {
-          for_each = var.config.azure_devops_organization.permission.administrator_account != null ? { this = {} } : {}
+          for_each = permission.value.administrator_account != null ? [permission.value.administrator_account] : []
           content {
-            groups = var.config.azure_devops_organization.permission.administrator_account.groups
-            users  = var.config.azure_devops_organization.permission.administrator_account.users
+            groups = administrator_account.value.groups
+            users  = administrator_account.value.users
           }
         }
       }
